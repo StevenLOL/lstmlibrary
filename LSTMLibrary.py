51a42c6aa55a50214c66e0aa7cc78ca39df416de
softmaxllc <softmax.studios@hotmail.com>
2015-04-03 13:54:20 -0700

    Added all

--- LSTMLibrary.py
+++ LSTMLibrary.py
@@ -3,6 +3,9 @@ from networks.lstm_encoder_decoder import LSTMEncoderDecoder
 from utils import dataset
 import numpy as np
 
+# Load dataset
+params = dataset.load_dataset("input_dummy.txt", "output_dummy.txt")
+
 input_vocab_size = 50
 output_vocab_size = 100
 input_embed_size = 100
@@ -30,5 +33,16 @@ for predList in predictions:
 
 
 
+def clean_files(dir_name):
+    import os
+    directory = os.listdir(dir_name)
+    for filename in directory:
+        if filename[-3:] == 'pyc':
+            print '- ' + filename
+            os.remove("{0}/{1}".format(dir_name,filename))
 
-
+clean_files('./layer')
+clean_files('./networks')
+clean_files('.')
+clean_files('./units')
+clean_files('./utils')
--- utils/dataset.py
+++ utils/dataset.py
@@ -1,19 +1,38 @@
-from nltk.corpus import movie_reviews
-import nltk
 
-def generateDataset():
-    documents = [(list(movie_reviews.words(fileid)), category) for category in movie_reviews.categories() for fileid in movie_reviews.fileids(category)]
-    words = movie_reviews.words()
 
-    all_words = nltk.FreqDist(w.lower() for w in movie_reviews.words())
-    word_to_idx = { "<unk>" : 0}
-    idx_to_word = { 0 : "<unk>" }
+def load_dataset(input_path, output_path):
+    """ Loads a dataset given specified input and output path """
+    input_lines = open(input_path).readlines()
+    output_lines = open(output_path).readlines()
 
+    assert(len(input_lines) == len(output_lines))
+    in_set = generate_vocab_iterations(input_lines)
+    out_set = generate_vocab_sentences(output_lines)
+    return {'input' : in_set, 'output' : out_set}
+
+def generate_vocab_iterations(train_lines):
+    """ Generates vocab indeces and converts sentences to integer values """
+
+    word_to_idx = {}
+    idx_to_word = {}
     curr_index = 1
-    for word in words:
-        numOccurences = all_words[word.lower()]
-        if numOccurences > 10:
-            word_to_idx[word] = curr_index
-            idx_to_word[curr_index] = word.lower()
+
+    # First pass: generate vocab
+    for line in lines:
+        tokens = line.lower().replace(".", " <end>").split(" ")
+        for token in tokens:
+            word_to_idx[token] = curr_index
+            idx_to_word[curr_index] = token
             curr_index += 1
-    random.shuffle(documents)
+
+    sentences = []
+    # Second pass: generate vocab
+    for line in lines:
+        sentence = []
+        tokens = line.lower().replace(".", " <end>").split(" ")
+        for token in tokens:
+            sentence.append(word_to_idx[tokens])
+
+        sentences.append(sentence)
+
+    return {'sentences' : sentences, 'word_to_idx' : word_to_idx, 'idx_to_word' : idx_to_word}
